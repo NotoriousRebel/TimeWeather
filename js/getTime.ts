2d9c3a5b3176e9a783bece6a40630c2e3e241edf
NotoriousRebel <matt12299@yahoo.com>
2019-06-13 15:29:15 -0400

    Removed js directory.

--- js/getTime.ts
+++
@@ -1,9 +0,0 @@
-export function calcTime(epoch_time: string, lat: string, loc: string, lon: string) {
-    const geoTz = require('geo-tz');
-    const moment = require('moment-timezone');
-    const timezone:string = geoTz(lat,lon);
-    //Look up timezone with latitude and longitude
-    let time = moment().tz(timezone.toString());
-    time = time.format("hh:mm a");
-    return time;
-}
